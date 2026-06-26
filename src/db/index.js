7a8e61b2feada9d06992fcb6d273e6c8f1d797f0
Abhishek Wakchaure <abhishekwakchaure44@gmail.com>
2026-06-26 15:14:57 +0530

    Connecting mongodb atlas to project

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+
+    try{
+    console.log(process.env.MONGODB_URI);
+    console.log(DB_NAME);
+     const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+     console.log(
+       `MongoDB Connected!! DB HOST: ${connectionInstance.connection.host}`
+     );
+     console.log("Hello from Abhi");
+    }catch(error){
+        console.log("Mongodb connection error", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,29 @@
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path :'./env'
+})
+
+connectDB()
+
+
+
+//import express from "express";
+//
+//const app=express()
+//
+//(async () => {
+//try{
+//   await mongoose.connect(`${process.env.MONGODB_URI}/S{DB_NAME}`)
+//
+//   app.on("eroor",(error)=> {
+//   console.log("ERRR: ", error);
+//   throw error
+//   })
+// } catch(error){
+//    console.error("Error: ",error);
+//    throw error
+//}
+//})()
